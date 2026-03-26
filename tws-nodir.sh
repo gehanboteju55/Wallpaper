80845b63782bdfbe1286d98811320fc601136def
Gehan Boteju <gehanb25@gmail.com>
2026-03-25 23:23:40 -0400

    Update tws-nodir.sh
    Removes file path when prompting what wallpaper to choose.

--- tws-nodir.sh
+++ tws-nodir.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+shopt -s nullglob
+
 dir=""
 wallpapers=("$dir"/*)
 
 echo "Pick a wallpaper:"
 
-select wallpaper in "${wallpapers[@]}"; do
+select wallpaper in "${wallpapers[@]##*/}"; do
     if [[ -n "$wallpaper" ]]; then
-        osascript <<EOF
+        full="$dir/$wallpaper"
+
+         osascript <<EOF
 tell application "System Events"
-    set picture of every desktop to "$wallpaper"
+    set picture of every desktop to "$full"
 end tell
 EOF
         echo "Wallpaper set to: $wallpaper"
