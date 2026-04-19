8dc1772881ebc40941cb768360000fcc4e8825dd
Gehan Boteju <gehanb25@gmail.com>
2026-04-18 23:14:44 -0400

    Update tws-linux-nodir.sh

--- tws-linux-nodir.sh
+++ tws-linux-nodir.sh
@@ -12,13 +12,18 @@ echo "Pick a wallpaper:"
 
 select wallpaper in "${wallpapers[@]##*/}"; do
     if [[ -n "$wallpaper" ]]; then
-        full="$dir/$wallpaper"
 
+        full="$dir/$wallpaper"
         awww img $full --transition-type random --transition-fps 179
 
-        echo "Wallpaper set to: $wallpaper"
+        wal -i $full #sets current terminal colors not all
+
+        echo "Theme Updated"
         break
     else
         echo "Try again"
     fi
 done
+
+killall -SIGUSR2 waybar
+
